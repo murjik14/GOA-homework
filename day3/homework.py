8b376cba80d58753d17503b180dd2c6c009ae8d2
murjik14 <datomurjikneli10@gmail.com>
2024-02-26 13:48:50 +0400

    second commit

--- day3/homework.py
+++ day3/homework.py
@@ -1,2 +1,4 @@
 names = "veriko shako ana nugzari da deizi(zdagli)"
+pasword=("123")
+print(pasword)
 print(names)
---
+++ day6/homework.py
@@ -0,0 +1,6 @@
+num1 = int(input("please enter your number 1"))
+num2= int(input("please enter your number 2"))
+num3= int (input("please enter your number 3"))
+
+print(num1 + num2 + num3)
+
